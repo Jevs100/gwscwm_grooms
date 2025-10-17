6a311ef9b942a8b79cc018d35caf0fdef53f9566
jevs100 <joshuaevs10@gmail.com>
2025-10-17 23:26:00 +0100

    Add test endpoint for josh

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,5 @@
 """This is the main FastAPI application Module."""
 
-from typing import Dict
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import uvicorn
@@ -16,6 +15,9 @@ async def read_root():
 async def health():
     return {"status": "ok"}
 
+@app.get("/josh")
+async def read_josh():
+    return {"message": "Hello Josh"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
